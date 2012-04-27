4b144f4dc2b2ab9734f55165e2195f3a23bc0efc
yumingyang <yumingyang@xiaomi.com>
2012-04-27 19:57:38 +0800

    use MiuiPhone
    Change-Id: Id1e7bedb0fba8281075d85094df85218d278f0bd

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -22,3 +22,14 @@ fi
 if [ $1 = "Settings" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
 fi
+
+if [ $1 = "Phone" ];then
+    for file in `find $1/smali -name *.part`
+    do
+	filepath=`dirname $file`
+	filename=`basename $file .part`
+        dstfile="out/$filepath/$filename"
+        cat $file >> $dstfile
+    done
+fi
+
